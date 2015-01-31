ca3c34f5b162ba4837b1e7a8571c616dabdad967
Jacek Spolnik <jacek.spolnik@gmail.com>
2015-01-31 09:48:22 +0100

    - add further gradle samples

---
+++ gradle_doc/chapter7/src/main/java/com/gradle/SimpleClass.java
@@ -0,0 +1,13 @@
+package com.gradle;
+
+public class SimpleClass {
+    private final String value;
+
+    public SimpleClass(String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+}
